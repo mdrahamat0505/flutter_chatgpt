5f8249d0f570e44feee0fbcea16627389d726706
Flutter Developer <56519330+mdrahamat0505@users.noreply.github.com>
2023-10-21 12:22:26 +0600

    update.

--- lib/secrets_example.dart
+++ lib/secrets_example.dart
@@ -1,2 +1,2 @@
-const openAiApiKey = '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';
+const openAiApiKey = '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';
 const openAiOrg = 'org-vBdWBCfGiZXBsXyQUxhlqDPx';
